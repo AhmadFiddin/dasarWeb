da99f6eb73c50f7c0534d5c4e92a06848ace17af
Ahmad Mimna'i Fiddin <ahmadfiddin687@gmail.com>
2024-04-01 09:17:59 +0700

    Pertemuan 07 - Praktikum 1 - 2

---
+++ empty.php
@@ -0,0 +1,14 @@
+<?php
+$myArray = array(); // Array kosong
+if (empty($myArray)) {
+    echo "Array tidak didefinisikan atau kosong.<br>";
+} else {
+    echo "Array terdefinisi dan tidak kosong.<br>";
+}
+
+if (empty($nonEexictentVar)) {
+    echo "Variabel tidak terdefinisi atau kosong.<br>";
+} else {
+    echo "Variabel terdefinisi dan tidak kosonf.<br>";
+}
+?>
